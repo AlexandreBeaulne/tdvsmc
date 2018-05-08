64ac1970ffefe97817fd01a989ec1429b892093d
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2018-05-08 09:22:12 -0400

    save images to make gif

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,9 @@
 
 import argparse
 from random import randint
-from statistics import mean
 
+import gym
+import matplotlib.pyplot as plt
 import torch
 from torch.autograd import Variable as Var
 import torch.nn.functional as F
@@ -14,42 +15,40 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, help='pytorch serialized model')
+    parser.add_argument('--num-frames', type=int, default=1000)
     args = parser.parse_args()
 
     name, _ext = args.model.split('.')
     algo, game, num_steps = name.split('_')
 
-    rewards = []
+    env = create_atari_env(game)
+    env.seed(3434)
+    env2 = gym.make('SeaquestDeterministic-v4')
+    env2.seed(3434)
+    env2.reset()
 
-    for i in range(1):
+    model = ActorCritic(env.observation_space.shape[0], env.action_space.n)
+    model.eval()
+    model.load_state_dict(torch.load(args.model))
+    cx = Var(torch.zeros(1, 256), volatile=True)
+    hx = Var(torch.zeros(1, 256), volatile=True)
 
-        env = create_atari_env(game)
+    state = env.reset()
+    state = torch.from_numpy(state)
+    done = False
 
-        model = ActorCritic(env.observation_space.shape[0], env.action_space.n)
-        model.eval()
-        model.load_state_dict(torch.load(args.model))
-        cx = Var(torch.zeros(1, 256), volatile=True)
-        hx = Var(torch.zeros(1, 256), volatile=True)
-
-        state = env.reset()
+    for i in range(args.num_frames):
+        cx, hx = Var(cx.data, volatile=True), Var(hx.data, volatile=True)
+        _, logit, (hx, cx) = model((Var(state.unsqueeze(0), volatile=True), (hx, cx)))
+        prob = F.softmax(logit)
+        action = prob.max(1, keepdim=True)[1].data.numpy()
+        state, _reward, _done, _ = env.step(action[0, 0])
+        img, _, _, _ = env2.step(action[0, 0])
+        plt.imsave('images/seaquest_a3c_{}'.format(i), img)
         state = torch.from_numpy(state)
-        rewards.append(0)
-        done = False
-
-        while not done:
-            cx, hx = Var(cx.data, volatile=True), Var(hx.data, volatile=True)
-            _, logit, (hx, cx) = model((Var(state.unsqueeze(0), volatile=True), (hx, cx)))
-            prob = F.softmax(logit)
-            action = prob.max(1, keepdim=True)[1].data.numpy()
-            state, reward, done, _ = env.step(action[0, 0])
-            rewards[-1] += reward
-            state = torch.from_numpy(state)
-            env.render()
+        env2.render()
 
     env.close()
-
-    print('algo,game,rollout,avg_score')
-    print('{},{},{},{}'.format(algo, game, num_steps, mean(rewards)))
 
 
 
